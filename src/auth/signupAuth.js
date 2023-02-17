be6efca178004fc6b1fa2cd71ea45513ec78014b
Danilo Leão <olin_ad@hotmail.com>
2023-02-17 15:00:47 -0300

    Fixing: updating signupAuth

--- src/auth/signupAuth.js
+++ src/auth/signupAuth.js
@@ -2,13 +2,14 @@ import jwt from 'jsonwebtoken';
 import { INTERNAL_SERVER_ERROR } from '../utils/statusCode.utils.js';
 
 async function signupAuth(request, response, next) {
-  const { password } = request.body;
+  const { password, email } = request.body;
   const payload = {
     password,
   };
 
   try {
     const token = jwt.sign(payload, 'secretkey');
+
 
     response.locals.token = token;
 
