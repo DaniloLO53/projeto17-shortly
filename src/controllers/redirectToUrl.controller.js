f91b4336090d4015ebf8648d8181e6b5d7b8a445
Danilo Leão <olin_ad@hotmail.com>
2023-02-20 11:48:30 -0300

    Updating: increase visitants counter

--- src/controllers/redirectToUrl.controller.js
+++ src/controllers/redirectToUrl.controller.js
@@ -1,18 +1,30 @@
 import { db } from "../config/database.connection.js";
 import { INTERNAL_SERVER_ERROR, NOT_FOUND } from "../utils/statusCode.utils.js";
 
+async function increaseVisitants(url) {
+  ++count;
+  await db.query(`
+    UPDATE shortens
+    SET visitcount = visitcount + 1
+    WHERE shorturl = $2
+  `, [url]);
+};
+
 async function redirectToUrl(request, response, next) {
   const { shortUrl } = request.params;
 
   try {
     const resultsFromShortens = await db.query(`
-      SELECT url
+      SELECT url, visitCount
       FROM shortens
       WHERE shorturl = $1
     `, [shortUrl]);
     const responseObj = resultsFromShortens.rows[0];
 
-    if (responseObj) return response.redirect(responseObj.url);
+    if (responseObj) {
+      increaseVisitants();
+      return response.redirect(responseObj.url);
+    }
 
     return response.sendStatus(NOT_FOUND);
 
@@ -21,8 +33,8 @@ async function redirectToUrl(request, response, next) {
 
     return response.sendStatus(INTERNAL_SERVER_ERROR);
   }
-}
+};
 
 export {
   redirectToUrl,
-}
+};
